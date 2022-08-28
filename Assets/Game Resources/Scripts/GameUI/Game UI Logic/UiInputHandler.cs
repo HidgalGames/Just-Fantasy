4e651331ebf209f1a4a5ad72af19c5424b6af7e7
Kirill.Zverev <kzverevsf@yandex.ru>
2022-08-28 17:41:42 +0700

    Added alt state for cursor

--- Assets/Game Resources/Scripts/GameUI/Game UI Logic/UiInputHandler.cs
+++ Assets/Game Resources/Scripts/GameUI/Game UI Logic/UiInputHandler.cs
@@ -1,20 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Zenject;
 
 namespace GameUI.Logic
 {
     public class UiInputHandler : MonoBehaviour
     {
+        private const string INVENTORY_BUTTON_NAME = "Inventory Button";
+
+        [SerializeField] private CursorController _cursorController;
         [SerializeField] private GameWindowsHandler _windowsHandler;
 
-        private const string INVENTORY_BUTTON_NAME = "Inventory Button";
+        [Inject] private CameraController _cameraController;
 
         private void Update()
         {
             if (Input.GetButtonDown(INVENTORY_BUTTON_NAME))
             {
                 _windowsHandler.SwitchWindowState("InventoryWindow");
+            }
+
+            HandleCursorAltState();
+        }
+
+        private void HandleCursorAltState()
+        {
+            if (Input.GetKeyDown(KeyCode.LeftAlt))
+            {
+                _cameraController.SetBlocked(true);
+                _cursorController.ShowCursor();
+            }
+
+            if (Input.GetKeyUp(KeyCode.LeftAlt))
+            {
+                _cameraController.SetBlocked(false);
+                _cursorController.HideCursor();
             }
         }
     }
